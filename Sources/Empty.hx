48ba6cf3749c5ca9ca64d1c170129db011b7425e
Christian Reuter <gamedev@aoe-maniac.de>
2015-08-18 18:01:36 +0200

    Added matrix usage.

--- Sources/Empty.hx
+++ Sources/Empty.hx
@@ -14,6 +14,7 @@ import kha.graphics4.VertexBuffer;
 import kha.graphics4.VertexData;
 import kha.graphics4.VertexShader;
 import kha.graphics4.VertexStructure;
+import kha.math.Matrix4;
 
 class Empty extends Game {
 
@@ -38,8 +39,10 @@ class Empty extends Game {
 	var vertexBuffer:VertexBuffer;
 	var indexBuffer:IndexBuffer;
 	var offsetBuffer:ArrayBuffer;
+	var mBuffer:ArrayBuffer;
 	var program:Program;
 	var offID:AttributeLocation;
+	var mID:AttributeLocation;
 
 	public function new() {
 		super("Empty");
@@ -86,6 +89,7 @@ class Empty extends Game {
 		offsetBuffer = new ArrayBuffer(
 			offsets.length,
 			3, // Vec3
+			1,
 			Usage.StaticUsage
 		);
 		
@@ -95,7 +99,42 @@ class Empty extends Game {
 		}
 		offsetBuffer.unlock();
 		
+		// Matrix for transformations
+		mBuffer = new ArrayBuffer(
+			3 * 4 * 4, // 3 * 4x4
+			4, // Vec4
+			4, // * 4 (there are no matrix primitives for glVertexAttribPointer)
+			Usage.StaticUsage
+		);
+		
+		var mData = mBuffer.lock();
+		var m : Matrix4;
+		for (i in 0...3) {
+			m = Matrix4.rotationZ(Math.PI * 0.5);
+			mData[i * 16 + 0] = m._00;
+			mData[i * 16 + 1] = m._01;
+			mData[i * 16 + 2] = m._02;
+			mData[i * 16 + 3] = m._03;
+			
+			mData[i * 16 + 4] = m._10;
+			mData[i * 16 + 5] = m._11;
+			mData[i * 16 + 6] = m._12;
+			mData[i * 16 + 7] = m._13;
+			
+			mData[i * 16 + 8] = m._20;
+			mData[i * 16 + 9] = m._21;
+			mData[i * 16 + 10] = m._22;
+			mData[i * 16 + 11] = m._23;
+			
+			mData[i * 16 + 12] = m._30;
+			mData[i * 16 + 13] = m._31;
+			mData[i * 16 + 14] = m._32;
+			mData[i * 16 + 15] = m._33;
+		}
+		mBuffer.unlock();
+		
 		offID = program.getAttributeLocation("off"); // Attribute location since it is changed during shader calls
+		mID = program.getAttributeLocation("m");
     }
 
 	override public function render(frame:Framebuffer) {
@@ -108,6 +147,7 @@ class Empty extends Game {
 		// Instanced rendering
 		if (g.instancedRenderingAvailable()) {
 			offsetBuffer.set(offID, 1); // Divisor is 1, i.e. offset changes after each instance is drawn
+			mBuffer.set(mID, 1); // Divisor is 1, i.e. offset changes after each instance is drawn
 			
 			g.setVertexBuffer(vertexBuffer);
 			g.setIndexBuffer(indexBuffer);
